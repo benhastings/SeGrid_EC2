42e5273e2136d95bf200249a0143115cf3c25ee2
benhastings <ben.hastings@gmail.com>
2013-02-08 13:22:54 -0500

    Update capacityTest.py

--- capacityTest.py
+++ capacityTest.py
@@ -7,15 +7,19 @@ iterations=int(sys.argv[1])
 size=sys.argv[2]
 hub=sys.argv[3]
 
-
+loop=5
+iter=0
 browsers=1
-while (browsers < iterations):
-        print('browsers:'+str(browsers))
-        #Popen('python gridExecuteCapacity.py '+str(browsers)+' 0 www.sciencedirect.com 5 '+hub+' >> '+size+'-'+str(browsers), shell=True).wait()
-        #Popen('python gridExecuteCapacity.py '+str(browsers)+' 0 cdc311-www.sciencedirect.com 5 '+hub+' '+size+' >> '+size, shell=True).wait()
-        Popen('python gridExecuteCapacity.py '+str(browsers)+' 0 cdc311-www.sciencedirect.com 5 '+hub+' '+size+' >> InstanceCapTest', shell=True).wait()
-        browsers=browsers+1
-        if browsers < iterations:
-                print(' rest for 10 seconds')
-                time.sleep(10)
+while (iter < loop):
+        while (browsers < iterations):
+                print('browsers:'+str(browsers))
+                #Popen('python gridExecuteCapacity.py '+str(browsers)+' 0 www.sciencedirect.com 5 '+hub+' >> '+size+'-'+str(browsers), shell=True).wait()
+                #Popen('python gridExecuteCapacity.py '+str(browsers)+' 0 cdc311-www.sciencedirect.com 5 '+hub+' '+size+' >> '+size, shell=True).wait()
+                Popen('python gridExecuteCapacity.py '+str(browsers)+' 0 www.sciencedirect.com 15 '+hub+' '+size+' >> InstanceCapTest', shell=True).wait()
+                browsers=browsers+1
+                if browsers < iterations:
+                        print(' rest for 10 seconds')
+                        time.sleep(10)
+        iter=iter+1
+        time.sleep(300)
 
