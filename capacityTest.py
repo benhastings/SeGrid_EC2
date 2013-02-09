257137821e35c663966c2df8cc0d72a1bdad396c
benhastings <ben.hastings@gmail.com>
2013-02-09 02:32:34 +0000

    update capacityTest.py script

--- capacityTest.py
+++ capacityTest.py
@@ -13,8 +13,6 @@ while (iter < loop):
 	browsers=1
         while (browsers < iterations):
                 print('browsers:'+str(browsers))
-                #Popen('python gridExecuteCapacity.py '+str(browsers)+' 0 www.sciencedirect.com 5 '+hub+' >> '+size+'-'+str(browsers), shell=True).wait()
-                #Popen('python gridExecuteCapacity.py '+str(browsers)+' 0 cdc311-www.sciencedirect.com 5 '+hub+' '+size+' >> '+size, shell=True).wait()
                 Popen('python gridExecuteCapacity.py '+str(browsers)+' 0 www.sciencedirect.com 15 '+hub+' '+size+' >> InstanceCapTest', shell=True).wait()
                 browsers=browsers+1
                 if browsers < iterations:
