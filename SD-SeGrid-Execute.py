ac50de7797a75db1b5cdc6f6a816b644176dfd0c
benhastings <ben.hastings@gmail.com>
2013-07-12 16:22:34 -0400

    Update SD-SeGrid-Execute.py
    update script

--- SD-SeGrid-Execute.py
+++ SD-SeGrid-Execute.py
@@ -1,3 +1,4 @@
+
 from subprocess import Popen
 import sys
 import urllib2
@@ -8,21 +9,25 @@ PHOST=resp.read()
 
 # Poll Hub interface to determine free/busy status of resources
 try:
-	response=urllib2.urlopen('http://localhost:4200/grid/console')
-	html=response.read()
-	exit
+        response=urllib2.urlopen('http://localhost:4200/grid/console')
+        html=response.read()
+        exit
 except urllib2.URLError:
-	pass
+        pass
+
+runningCount = html.count("class='busy'")
+freeCount = html.count("platform=")
 
 resp=urllib2.urlopen('http://169.254.169.254/latest/meta-data/public-hostname')
 PHOST=resp.read()
-	
+
 # If resources available (first condition) add more requests
 #   - rename whatever test you want to run to the 'sdtest.py' value
 # Otherwise, exit and wait a while
-if 'type=WebDriver' in html:
-	Popen('python gridExecute.py 5 0 sdtest.py 4000000 '+PHOST+'&',shell=True)
+#if 'type=WebDriver' in html:
+if freeCount > 0:
+        Popen('python gridExecute.py ' + str(freeCount) + ' 0 sdtest.py 4000000 '+PHOST+'&',shell=True)
 elif 'requests waiting for a slot to be free' in html:
-	exit
+        exit
 else:
-	exit
+        exit
