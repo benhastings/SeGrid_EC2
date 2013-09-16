42ec7ec2f422dcc2562b5827c68fb5aed5df00f9
benhastings <ben.hastings@gmail.com>
2013-09-16 17:21:08 -0400

    Update SD-SeGrid-Execute.py

--- SD-SeGrid-Execute.py
+++ SD-SeGrid-Execute.py
@@ -5,7 +5,7 @@ import time
 # Find hostname to use for passing to webdriver
 resp=urllib2.urlopen('http://169.254.169.254/latest/meta-data/public-hostname')
 PHOST=resp.read()
-#PHOST='localhost'
+PHOST='localhost'
 
 # Poll Hub interface to determine free/busy status of resources
 def freeCheck():
@@ -27,12 +27,7 @@ def freeCheck():
 #if 'type=WebDriver' in html:
 freeCount=freeCheck()
 while freeCount>0:
-        if (freeCount > 5):
-                Popen('python gridExecute.py 5 0 sdtest.py 40000 '+PHOST+'&',shell=True,close_fds=True)
-        elif (freeCount > 2):
-                Popen('python gridExecute.py 2 0 sdtest.py 40000 '+PHOST+'&',shell=True,close_fds=True)
-        else:
-                Popen('python gridExecute.py 1 0 sdtest.py 40000 '+PHOST+'&',shell=True,close_fds=True)
+        Popen('python gridExecute.py 2 0 sdtest.py 40000 '+PHOST+'&',shell=True,close_fds=True)
         
         time.sleep(60)
         try:
