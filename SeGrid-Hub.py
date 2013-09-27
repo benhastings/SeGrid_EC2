0a8e4802121ac6404bf985555dee86aff3359c72
benhastings <ben.hastings@gmail.com>
2013-09-27 14:38:42 +0000

    update hubstart logic

--- SeGrid-Hub.py
+++ SeGrid-Hub.py
@@ -7,12 +7,14 @@ html=''
 try:
         response=urllib2.urlopen('http://localhost:4200/grid/console')
         html=response.read()
+	#-- If SeGrid Hub available, move on, if not, start it
+	if 'Grid Console' in html:
+        	exit
+	else:
+        	Popen('java -jar /home/ubuntu/selenium-server.jar -role hub -port 4200 -DPOOL_MAX=256 -timeout 15 -browserTimeout 60 -cleanUpCycle 3000&',shell=True)
         exit
-except urllib2.URLError:
-        pass
+except urllib2.ULError:
+        exit
+except urllib2.HTTPError:
+        exit
 
-#-- If SeGrid Hub available, move on, if not, start it
-if 'Grid Console' in html:
-        exit
-else:
-        Popen('java -jar /home/ubuntu/selenium-server.jar -role hub -port 4200 -DPOOL_MAX=256 -timeout 15000 -browserTimeout 60 -cleanUpCycle 1000&',shell=True)
