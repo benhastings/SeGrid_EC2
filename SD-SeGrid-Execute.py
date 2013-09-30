45c971cb651f0d36e654b2a4e9f8d53573aba3db
benhastings <ben.hastings@gmail.com>
2013-09-30 14:34:11 +0000

    updated execute script with count logic

--- SD-SeGrid-Execute.py
+++ SD-SeGrid-Execute.py
@@ -9,15 +9,19 @@ PHOST='localhost'
 
 # Poll Hub interface to determine free/busy status of resources
 def freeCheck():
-        try:
+        free=0
+	wait=0
+	try:
                 response=urllib2.urlopen('http://localhost:4200/grid/console')
                 time.sleep(2)
 		html=response.read()
 		# Check to see if any browsers are available
 		free = html.count("platform=")
+		#print 'free:'+str(free)
 		# Check to see if any scripts are queued
 		wait = html.count("waiting")
-                #exit
+                #print 'wait:'+str(wait)
+		#exit
         except urllib2.URLError:
                 count = 0
         except urllib2.HTTPError:
@@ -28,12 +32,12 @@ def freeCheck():
 	if wait > 0:
 		count=0
 	# if slots are free, proceed
-	else if free > 0:
+	elif free > 0:
 		count=free
 	# otherwise, do nothing further
 	else:
 		count=0
-
+	#print 'return count of:'+str(count)
         return count
 
 # If resources available (first condition) add more requests
@@ -42,9 +46,12 @@ def freeCheck():
 #if 'type=WebDriver' in html:
 freeCount=0
 freeCount=freeCheck()
+#print 'freeCount before loop:'+str(freeCount)
 while freeCount>1:
+	#print 'I have entered the loop'
         Popen('python gridExecute.py 2 0 sdtest.py 400 '+PHOST+'&',shell=True,close_fds=True)
 	try:
+		#print 'I am trying to start more tests'
 		url2Send = urllib2.urlopen('http://cert-pa.elsevier.com/perfTest?perfTest.cpc=SD&perfTest.cpc.newScripts=2')        
         	time.sleep(30)
         	try:
@@ -57,6 +64,7 @@ while freeCount>1:
 			exit
 	
         except:
+		#print 'something failed, so leaving'
                 freeCount=0
                 time.sleep(30)
 		exit
