38eeced19399cd604e9fda18f87571e5d05fe926
benhastings <ben.hastings@gmail.com>
2014-09-03 22:28:38 +0000

    updated nodeStart to cleanup chrome instances

--- nodeStart.sh
+++ nodeStart.sh
@@ -1,4 +1,6 @@
 #/bin/bash
 sleep 10
 
+sudo pkill chrome
+
 sudo xvfb-run --server-args='+extension RANDR -screen 0 1280x800x24' java -jar /home/ubuntu/selenium-server.jar -role node -browser browserName=chrome,maxInstances=5 -maxSession 5 -port 4320 -hub http://seGrid-714528302.us-east-1.elb.amazonaws.com/grid/register -registerCycle 5000 &
